a7fbf2e0735af85181e8dfdf40895059bc23cd76
romel <romelmacinas123@gmail.com>
2026-09-20 14:17:03 +0800

    Updated code changes

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -11,7 +11,7 @@
                             <div class="detail-box">
                                 <h1>
                                     <span>
-                                        Thirsty?
+                                        lover?
                                     </span>
                                     <br>
                                     Have a Drink
